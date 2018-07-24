05b4db729cfdcc7ebd1f4ec0083e3ccda2e56480
mionsek <mionskowski.dawid@gmail.com>
2018-07-24 08:48:03 +0200

    Dodanie przyciskow samochodow
    Same przyciski, bez obsługi

--- Srednie_spalanie/Form1.cs
+++ Srednie_spalanie/Form1.cs
@@ -26,6 +26,7 @@ namespace Srednie_spalanie
             this._samochody = samochody;
 
             InitializeComponent(_liczba_samochodow, _samochody);
+            CreateDynamicButtons();
         }
 
         private void rej1_button_Click(object sender, EventArgs e)
@@ -35,5 +36,36 @@ namespace Srednie_spalanie
             myForm.ShowDialog();
             this.Close();
         }
+
+        private void CreateDynamicButtons()
+        {
+            int tmp_cnt = 0;
+            for (int i = 0; i < this._liczba_samochodow; i++)
+            {
+                int margin = 6;
+                int height = 37;
+                int width = 350;
+                Button dynamicButton = new Button();
+                // Set Button properties
+                dynamicButton.Height = height;
+                dynamicButton.Width = width;
+                if (tmp_cnt % 2 == 0)
+                {
+                    dynamicButton.Location = new Point(ClientSize.Width / 2 - (width + margin), 70 + (tmp_cnt / 2 * height) + margin * i);
+                }
+                else
+                {
+                    dynamicButton.Location = new Point(ClientSize.Width / 2 + margin, 70 + (tmp_cnt/2 * height) + margin*(i - 1));
+                }
+                dynamicButton.Text = this._samochody[i].Nr_rej + " - " + this._samochody[i].Nazwa;
+                dynamicButton.Name = "SamochodButton" + i;
+                dynamicButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
+                dynamicButton.UseVisualStyleBackColor = true;
+
+                Controls.Add(dynamicButton);
+                tmp_cnt += 1;
+            }
+        }
+
     }
 }
